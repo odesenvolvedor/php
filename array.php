99dad706f9c244f90e309863dc52523c4d36f4e9
Fernando Campos de Oliveira <fernando@odesenvolvedor.net>
2020-03-03 19:38:44 -0300

    Create array.php

---
+++ array.php
@@ -0,0 +1,45 @@
+<?php
+$arvoreGenealogica = [
+    [
+        "nome" => "João",
+        "filhos" => [
+            [
+                "nome" => "José",
+                "filhos" => [
+                    ["nome" => "Maria"],
+                ],
+            ],
+
+            [
+                "nome" => "Helena",
+                "filhos" => [
+                    [
+                        "nome" => "Vitor",
+                        "filhos" => [
+                            "nome" => "Ellen",
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    [
+        "nome" => "Yasmin",
+        "filhos" => [
+            [
+                "nome" => "Evellyn",
+                "filhos" => [
+                    ["nome" => "Cristinny"],
+                ],
+            ],
+
+            [
+                "nome" => "Elisa",
+                "filhos" => [
+                    ["nome" => "Aline"],
+                ],
+            ],
+        ],
+    ],
+];
